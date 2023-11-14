e31e42ae7c9fa244bf9e70df0d6a3b93510e85c5
mriggle <mriggle@ucls.uchicago.edu>
2023-11-14 14:23:46 -0600

    remove stuff

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,13 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "debug_toolbar",
-    'hijack',
-    "hijack.contrib.admin",
 ]
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'hijack.middleware.HijackUserMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
